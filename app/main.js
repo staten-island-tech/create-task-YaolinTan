488dac04c9b9454dd2c0d1a558c3979effee5a6d
YaolinTan <yaolint@nycstudents.net>
2024-12-17 12:31:16 -0500

    e

--- app/main.js
+++ app/main.js
@@ -1,13 +1,1 @@
 console.log("Hello World");
-
-import { BalldontlieAPI } from "@balldontlie/sdk";
-
-const api = new BalldontlieAPI({
-  apiKey: "4d7af431-d6a9-403d-a99e-571a637f0be6",
-});
-const stats = await api.nba.getSeasonAverages({
-  season: 2023,
-  player_id: 1996,
-});
-
-console.log(stats);
