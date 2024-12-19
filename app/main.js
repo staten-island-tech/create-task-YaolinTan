446f69be7151f48e3a2eeac8094d566ff3ca5dcf
YaolinTan <yaolint@nycstudents.net>
2024-12-19 12:31:47 -0500

    e

--- app/main.js
+++ app/main.js
@@ -1,31 +1,6 @@
 console.log("Hello World");
+import { players } from "./array";
 
-const fetchPlayer = async () => {
-  const url = `https://cors-anywhere.herokuapp.com/https://api.balldontlie.io/v1/teams`;
-  const options = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "4d7af431-d6a9-403d-a99e-571a637f0be6",
-      "x-rapidapi-host": "https://www.balldontlie.io/",
-    },
-  };
+let shotsMade = 0;
 
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      console.error(
-        `Failed to fetch data. Status: ${response.status}. Please try again later`
-      );
-      return null;
-    }
-
-    const data = await response.json();
-    console.log(`Data for Teams:`, data);
-    return data;
-  } catch (error) {
-    console.error(`Error fetching data. Please try again later`);
-    return null;
-  }
-};
-
-fetchPlayer();
+while (shotsMade < 10) {}
