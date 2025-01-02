25097be86c38966c41cff9c9c25705d2691c7aa9
YaolinTan <yaolint@nycstudents.net>
2025-01-01 22:00:02 -0500

    e

--- app/main.js
+++ app/main.js
@@ -1,13 +1,140 @@
 console.log("Hello World");
 import { players } from "./array";
 
-let shotsMade = 0;
+const DOMselectors = {
+  button: document.querySelectorAll(".button"),
+  button1: document.querySelector(".button1"),
+  button2: document.querySelector(".button2"),
+  button3: document.querySelector(".button3"),
+};
 
-while (shotsMade < 10) {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  let three = players[`${randomNumber}`].threePt;
-  let two = players[`${randomNumber}`].twoPt;
-  let one = players[`${randomNumber}`].freeThrow;
+let deficit = 2;
 
-  shotsMade++;
+async function startGame() {
+  while (deficit > -1) {
+    const playerContainer = document.querySelector(".player");
+    const consoleContainer = document.querySelector(".console");
+    playerContainer.innerHTML = "";
+    const randomNumber = Math.floor(Math.random() * 99);
+    let player = players[randomNumber];
+    console.log(player.name);
+    let three = player.threePt;
+    console.log("3pt%", three);
+    let two = player.twoPt;
+    console.log("2pt%:", two);
+    let one = player.freeThrow;
+    console.log("ft%:", one);
+    insertPlayer(player, playerContainer);
+    const shotTaken = Math.random() * 1;
+    console.log(shotTaken);
+
+    const { shotSelected, points } = await waitForClick(three, two, one);
+    const contextContainer = document.querySelector(".contextRow");
+    results(shotTaken, points, shotSelected);
+    insertConsole(deficit, player, consoleContainer, contextContainer);
+  }
+}
+
+startGame();
+
+function waitForClick(three, two, one) {
+  return new Promise((resolve) => {
+    const buttons = [
+      DOMselectors.button1,
+      DOMselectors.button2,
+      DOMselectors.button3,
+    ];
+
+    buttons.forEach((button) => {
+      button.addEventListener(
+        "click",
+        (event) => {
+          event.preventDefault();
+          let shotSelected, points;
+
+          if (button === DOMselectors.button1) {
+            shotSelected = one * one;
+            points = 2;
+            console.log("2ft%:", shotSelected);
+          } else if (button === DOMselectors.button2) {
+            shotSelected = two;
+            points = 2;
+          } else if (button === DOMselectors.button3) {
+            shotSelected = three;
+            points = 3;
+          }
+
+          resolve({ shotSelected, points });
+        },
+        { once: true }
+      );
+    });
+  });
+}
+
+function results(shotTaken, points, shotSelected) {
+  if (shotTaken <= shotSelected) {
+    console.log(deficit, points);
+    deficit -= points;
+    console.log("Splash");
+    console.log(deficit);
+  } else {
+    console.log("Clank");
+  }
+}
+
+function insertPlayer(player, playerContainer) {
+  playerContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div></div>
+      <h2>${player.name}</h2>
+      <div class="text">
+      <h3>Three Point Percentage:</h3>
+      <p>${player.threePt}</p>
+      </div>
+      <div class="text">
+      <h3>Two Point Percentage:</h3>
+      <p>${player.twoPt}</p>
+      </div>
+      <div class="text">
+      <h3>Free throw Percentage:</h3>
+      <p>${player.freeThrow}</p>
+      </div>
+    `
+  );
+}
+
+let i = 1;
+
+function insertConsole(deficit, player, consoleContainer, contextContainer) {
+  if (deficit === 2) {
+    contextContainer.innerHTML = "";
+    contextContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+       <div class="context card"><h2>Rebound Bosh, back out to the perimeter</h2></div>
+      `
+    );
+  }
+
+  if (deficit === 0) {
+    contextContainer.innerHTML = "";
+    contextContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="context card"><h2>Time runs out and it ends in a tie, now you're in the last possession of OT ${i} with the ball in your hands again. Pick the shot you take carefully as it could decide the outcome of the game </h2></div>
+    `
+    );
+    i += 1;
+  }
+  if (deficit < 0) {
+    consoleContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+    <h2>*Buzzer sounds* Is this the dagger... Bang! Bang! Game! Series! ${player.name} seals the game </h2>
+    `
+    );
+    return "splash";
+  }
 }
